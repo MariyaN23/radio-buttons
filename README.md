50c9bfda8768d6e2172dec0948ded2537e024a66
MariyaN23 <sellaite505@gmail.com>
2025-02-02 18:31:47 +0300

    init

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # 🔘 Radio buttons 🔘
 
 ## Description
+
 Radio button component with different custom icons for options \
 with using React and Tailwind CSS
--- src/Radio.tsx
+++ src/Radio.tsx
@@ -1,52 +1,67 @@
-import {ChangeEvent, ComponentPropsWithoutRef, createContext, ReactNode, useContext} from "react";
+import {
+  ChangeEvent,
+  ComponentPropsWithoutRef,
+  createContext,
+  ReactNode,
+  useContext,
+} from 'react';
 
 type RadioProps = {
-    children: ReactNode
-} & ComponentPropsWithoutRef<'input'>
+  children: ReactNode;
+} & ComponentPropsWithoutRef<'input'>;
 
 type RadioGroupProps = {
-    checkedIcon?: ReactNode
-    children: ReactNode
-    icon?: ReactNode
-    onChange: (event: ChangeEvent<HTMLInputElement>) => void
-    value: string
-}
+  checkedIcon?: ReactNode;
+  children: ReactNode;
+  icon?: ReactNode;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+};
 
 type RadioContextType = {
-    checkedIcon?: ReactNode
-    icon?: ReactNode
-    onChange: (event: ChangeEvent<HTMLInputElement>) => void
-    value: string
-}
+  checkedIcon?: ReactNode;
+  icon?: ReactNode;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+};
 
-const RadioContext = createContext<RadioContextType | null>(null)
+const RadioContext = createContext<RadioContextType | null>(null);
 
-export const Radio = ({children, ...props}: RadioProps) => {
-    const context = useContext(RadioContext)
-    if (!context) {
-        throw new Error(
-            "RadioContext has to be used within <RadioContext.Provider>"
-        )
-    }
-    const {checkedIcon, icon, onChange, value} = context
-    return (
-        <label className={'cursor-pointer w-full flex gap-4 p-2 border border-dotted'}>
-            {value === props.value ? checkedIcon : icon}
-            <input
-                checked={value === props.value}
-                className={'hidden'}
-                onChange={onChange}
-                type={'radio'}
-                {...props}/>
-            {children}
-        </label>
-    )
-}
+export const Radio = ({ children, ...props }: RadioProps) => {
+  const context = useContext(RadioContext);
+  if (!context) {
+    throw new Error(
+      'RadioContext has to be used within <RadioContext.Provider>',
+    );
+  }
+  const { checkedIcon, icon, onChange, value } = context;
+  return (
+    <label
+      className={'flex w-full cursor-pointer gap-4 border border-dotted p-2'}
+    >
+      {value === props.value ? checkedIcon : icon}
+      <input
+        checked={value === props.value}
+        className={'hidden'}
+        onChange={onChange}
+        type={'radio'}
+        {...props}
+      />
+      {children}
+    </label>
+  );
+};
 
-export const RadioGroup = ({checkedIcon, children, icon, onChange, value}: RadioGroupProps) => {
-    return (
-        <RadioContext.Provider value={{checkedIcon, icon, onChange, value}}>
-            {children}
-        </RadioContext.Provider>
-    )
-}
+export const RadioGroup = ({
+  checkedIcon,
+  children,
+  icon,
+  onChange,
+  value,
+}: RadioGroupProps) => {
+  return (
+    <RadioContext.Provider value={{ checkedIcon, icon, onChange, value }}>
+      {children}
+    </RadioContext.Provider>
+  );
+};
